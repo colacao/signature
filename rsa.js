672c56afb8005d09167b019f4bbdc2a3ce8a5bb8
colacao <colacao@EBJ1186.local>
2016-02-24 00:44:35 +0800

    df

--- rsa.js
+++
@@ -1,8 +0,0 @@
-var NodeRSA = require('node-rsa');
-var key = new NodeRSA("a",{b: 512});
- 
-var text = 'Hello RSA!';
-var encrypted = key.encrypt(text, 'base64');
-console.log('encrypted: ', encrypted);
-var decrypted = key.decrypt(encrypted, 'utf8');
-console.log('decrypted: ', decrypted);
