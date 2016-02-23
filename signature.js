87a2496ade24894c9fb90af58320879f1886b9cc
colacao <colacao@EBJ1186.local>
2016-02-24 00:40:30 +0800

    df

--- signature.js
+++ signature.js
@@ -3,16 +3,21 @@ var fs=require('fs');
 var arguments = process.argv.splice(2);
 var zip_file = arguments[0];
 var zip_code =  md5File(zip_file);
-console.log(zip_code);
+//console.log(zip_code);
 fs.readFile("1package.json",'utf-8', function(err, data){
   var content = data.replace(/"key":(.*)/g,"\"key\":\""+""+"\",");
   content = content.replace(/"package_md5":(.*)/g,"\"package_md5\":\""+zip_code+"\",");
+  // console.log(content);
+   // fs.writeFile('2package.json', content, function(err) {
+
+   // });
   fs.writeFile('/tmp/test.json', content, function(err) {
     var ss = md5File("/tmp/test.json");
+    console.log(ss);
     fs.readFile("/tmp/test.json",'utf-8', function(err, data){
 
           var nc = data.replace(/"key":(.*)/g,"\"key\":\""+ss+"\",");
-          console.log(nc);
+          //console.log(nc);
           fs.writeFile('1package.json', nc, function(err) {
 
           });
