88bddc2123f572981a6df5c93459e5e49d98d621
colacao <colacao@EBJ1186.local>
2016-02-24 12:06:54 +0800

    df

--- signature.js
+++ signature.js
@@ -7,13 +7,20 @@ var zip_code =  md5File(zip_file);
 fs.readFile("1package.json",'utf-8', function(err, data){
   var content = data.replace(/"key":(.*)/g,"\"key\":\""+""+"\",");
   content = content.replace(/"package_md5":(.*)/g,"\"package_md5\":\""+zip_code+"\",");
+  //content = content.replace(/"version":(.*)/g,"\"version\":\""+zip_code+"\",");
+  var vvv = content.match(/\"version\":.*/g)[0].match(/[0-9]\d*/g);
+  var oldVersion = (vvv.join('')*1).toString().split('').join('.')
+  var newVersion = (vvv.join('')*1+1).toString().split('').join('.');
+  content = content.replace(/"version":(.*)/g,"\"version\":\""+newVersion+"\",");
+
+  
   // console.log(content);
    // fs.writeFile('2package.json', content, function(err) {
 
    // });
   fs.writeFile('/tmp/test.json', content, function(err) {
     var ss = md5File("/tmp/test.json");
-    console.log(ss);
+    console.log(oldVersion);
     fs.readFile("/tmp/test.json",'utf-8', function(err, data){
 
           var nc = data.replace(/"key":(.*)/g,"\"key\":\""+ss+"\",");
--- test.sh
+++ test.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
+
+# 获取当前目录名
 dirName=`echo $(pwd)  | awk -F  \/ '{print $NF}'`
-tar -cf /tmp/$dirName.tar  --exclude .git/ --exclude ./$dirName.zip  --exclude  ./package.json ./
-cp /tmp/$dirName.tar  ./
-zip $dirName.zip $dirName.tar > /dev/null 2>&1
-rm $dirName.tar
-md5code=`md5 $dirName.zip | awk   '{print $NF}'`
-echo $md5code
+
+versionstr=`cat 1package.json  |  awk -F"[,:]" '{for(i=1;i<=NF;i++){if($i~/version\042/){print $(i+1)} } }'`
+# 获取版本号
+version=`echo $versionstr | sed 's/\"//g'`
+
+# 打包
+function package(){
+	if [ ! -f "$dirName.$version.zip" ]; then 
+		tar -cf /tmp/$dirName.tar  --exclude=".git/" --exclude="./$dirName.*.zip"  --exclude="./package.json" ./
+		cp /tmp/$dirName.tar  ./
+		zip $dirName.$version.zip $dirName.tar > /dev/null 2>&1
+		rm $dirName.tar
+	fi
+
+	md5code=`md5 $dirName.$version.zip | awk   '{print $NF}'`
+	newvsersion=`node signature.js $dirName.$version.zip`
+	echo $newvsersion
+}
+
+# 如果没有传入老zip
+if [ ! -n "$1" ] ;then
+    	package
+else
+	# 如果传入的参数的文件不存在
+	if [ ! -f "$1" ]; then 
+    		package
+	else
+		# 如果文件存在就打包且生成增量包
+		package
+		echo $1-------------$newvsersion
+		bsdiff $1 $dirName.$version.zip $dirName.$version.patch.zip
+	fi
+fi
